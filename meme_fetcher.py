9e0b7d0fbe06ab94cab15ad9021e9c0033475fd6
Maciej Czarkowski <mczarkowski98@gmail.com>
2021-05-27 16:23:52 +0200

    add phash to dictionary

--- meme_fetcher.py
+++ meme_fetcher.py
@@ -1,4 +1,7 @@
 import praw
+import imagehash
+from PIL import Image
+import requests
 
 reddit = praw.Reddit(
     client_id="hIcBbGcEmuy88w",
@@ -6,43 +9,47 @@ reddit = praw.Reddit(
     user_agent="my user agent",
 )
 
-tags = ['memes', 'meme','dunkmeme','topmemes']
+tags = ['memes', 'meme']
 
 
 def fetch_memes(quantity):
     unique_memes = []
     unique_memes_hashes = []
-    try:
-        for tag in tags:
-            for submission in reddit.subreddit(tag).hot(limit=quantity):
-                append_dictionary(unique_memes, submission, unique_memes_hashes)
-            for submission in reddit.subreddit(tag).new(limit=quantity):
-                append_dictionary(unique_memes, submission, unique_memes_hashes)
-            for submission in reddit.subreddit(tag).controversial(limit=quantity):
-                append_dictionary(unique_memes, submission, unique_memes_hashes)
-            for submission in reddit.subreddit(tag).rising(limit=quantity):
-                append_dictionary(unique_memes, submission, unique_memes_hashes)
-            for submission in reddit.subreddit(tag).top(limit=quantity):
-                append_dictionary(unique_memes, submission, unique_memes_hashes)
-    except:
-        print("Error occured")
+    for tag in tags:
+        for submission in reddit.subreddit(tag).hot(limit=quantity):
+            append_dictionary(unique_memes, submission, unique_memes_hashes)
+        for submission in reddit.subreddit(tag).new(limit=quantity):
+            append_dictionary(unique_memes, submission, unique_memes_hashes)
+        for submission in reddit.subreddit(tag).controversial(limit=quantity):
+            append_dictionary(unique_memes, submission, unique_memes_hashes)
+        for submission in reddit.subreddit(tag).rising(limit=quantity):
+            append_dictionary(unique_memes, submission, unique_memes_hashes)
+        for submission in reddit.subreddit(tag).top(limit=quantity):
+            append_dictionary(unique_memes, submission, unique_memes_hashes)
     return unique_memes
 
 
 def append_dictionary(unique_memes, submission, unique_memes_hashes):
-    if hash(submission) not in unique_memes_hashes and submission.url.endswith(".jpg"):
-        unique_memes.append({
-            'url': submission.url,
-            'name': submission.name,
-            'title': submission.title,
-            'timestamp': submission.created_utc,
-            'score': submission.score,
-            'upvote_ratio': submission.upvote_ratio
-        })
-        unique_memes_hashes.append(hash(submission))
+    try:
+        if hash(submission) not in unique_memes_hashes and submission.url.endswith(".jpg"):
+            unique_memes.append({
+                'url': submission.url,
+                'name': submission.name,
+                'title': submission.title,
+                'timestamp': submission.created_utc,
+                'score': submission.score,
+                'upvote_ratio': submission.upvote_ratio,
+                'phash': str(imagehash.phash(get_image_from_url(submission.url)))
+            })
+            unique_memes_hashes.append(hash(submission))
+    except:
+        print("Error occured")
 
 
-meme_dict = fetch_memes(1000)
-print(len(meme_dict))
-print(meme_dict[12])
+def get_image_from_url(url: str) -> Image:
+    return Image.open(requests.get(url, stream=True).raw)
+
+
+meme_dict = fetch_memes(5)
+print(f"Found {len(meme_dict)} unique memes")
 
