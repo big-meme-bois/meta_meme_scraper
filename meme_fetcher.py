020b0478054a1a4f5b779708e3368e23f1a26f48
Maciej Czarkowski <mczarkowski98@gmail.com>
2021-05-27 17:09:14 +0200

    Luke we love uuuuuuu, also added errors handling

--- meme_fetcher.py
+++ meme_fetcher.py
@@ -10,23 +10,26 @@ reddit = praw.Reddit(
     user_agent="my user agent",
 )
 
-tags = ['memes', 'meme']
+tags = ['memes', 'meme','trump']
 
 
 def fetch_memes(quantity):
     unique_memes = []
     unique_memes_hashes = []
     for tag in tags:
-        for submission in reddit.subreddit(tag).hot(limit=quantity):
-            append_dictionary(unique_memes, submission, unique_memes_hashes)
-        for submission in reddit.subreddit(tag).new(limit=quantity):
-            append_dictionary(unique_memes, submission, unique_memes_hashes)
-        for submission in reddit.subreddit(tag).controversial(limit=quantity):
-            append_dictionary(unique_memes, submission, unique_memes_hashes)
-        for submission in reddit.subreddit(tag).rising(limit=quantity):
-            append_dictionary(unique_memes, submission, unique_memes_hashes)
-        for submission in reddit.subreddit(tag).top(limit=quantity):
-            append_dictionary(unique_memes, submission, unique_memes_hashes)
+        try:
+            for submission in reddit.subreddit(tag).hot(limit=quantity):
+                append_dictionary(unique_memes, submission, unique_memes_hashes)
+            for submission in reddit.subreddit(tag).new(limit=quantity):
+                append_dictionary(unique_memes, submission, unique_memes_hashes)
+            for submission in reddit.subreddit(tag).controversial(limit=quantity):
+                append_dictionary(unique_memes, submission, unique_memes_hashes)
+            for submission in reddit.subreddit(tag).rising(limit=quantity):
+                append_dictionary(unique_memes, submission, unique_memes_hashes)
+            for submission in reddit.subreddit(tag).top(limit=quantity):
+                append_dictionary(unique_memes, submission, unique_memes_hashes)
+        except:
+            print("Submission errror")
     return unique_memes
 
 
@@ -51,6 +54,7 @@ def get_image_from_url(url: str) -> Image:
     return Image.open(requests.get(url, stream=True).raw)
 
 
-meme_dict = fetch_memes(2)
+meme_dict = fetch_memes(1000)
 print(f"Found {len(meme_dict)} unique memes")
+print(meme_dict[77])
 
