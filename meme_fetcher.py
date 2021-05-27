d48d6846af5dad032f66e3145c2c31c8ce8e8733
Maciej Czarkowski <mczarkowski98@gmail.com>
2021-05-27 15:31:25 +0200

    create appropriate output of fetcher (list of meme objects)

--- meme_fetcher.py
+++ meme_fetcher.py
@@ -6,15 +6,43 @@ reddit = praw.Reddit(
     user_agent="my user agent",
 )
 
+tags = ['memes', 'meme','dunkmeme','topmemes']
+
 
 def fetch_memes(quantity):
-    unique_memes_url = []
-    for submission in reddit.subreddit("meme").hot(limit=quantity):
-        if submission.url not in unique_memes_url and submission.url.endswith(".jpg"):
-            unique_memes_url.append(submission.url)
-    for item in unique_memes_url:
-        print(item)
-    return unique_memes_url
+    unique_memes = []
+    unique_memes_hashes = []
+    try:
+        for tag in tags:
+            for submission in reddit.subreddit(tag).hot(limit=quantity):
+                append_dictionary(unique_memes, submission, unique_memes_hashes)
+            for submission in reddit.subreddit(tag).new(limit=quantity):
+                append_dictionary(unique_memes, submission, unique_memes_hashes)
+            for submission in reddit.subreddit(tag).controversial(limit=quantity):
+                append_dictionary(unique_memes, submission, unique_memes_hashes)
+            for submission in reddit.subreddit(tag).rising(limit=quantity):
+                append_dictionary(unique_memes, submission, unique_memes_hashes)
+            for submission in reddit.subreddit(tag).top(limit=quantity):
+                append_dictionary(unique_memes, submission, unique_memes_hashes)
+    except:
+        print("Error occured")
+    return unique_memes
 
 
-fetch_memes(20)
+def append_dictionary(unique_memes, submission, unique_memes_hashes):
+    if hash(submission) not in unique_memes_hashes and submission.url.endswith(".jpg"):
+        unique_memes.append({
+            'url': submission.url,
+            'name': submission.name,
+            'title': submission.title,
+            'timestamp': submission.created_utc,
+            'score': submission.score,
+            'upvote_ratio': submission.upvote_ratio
+        })
+        unique_memes_hashes.append(hash(submission))
+
+
+meme_dict = fetch_memes(1000)
+print(len(meme_dict))
+print(meme_dict[12])
+
